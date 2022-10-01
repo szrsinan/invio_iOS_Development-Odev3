2f8f451dcb7fb1fd15bdb6b1d5a7ff5e6b6b6166
SinanSezer <sinaan.sezer1@gmail.com>
2022-10-01 11:47:01 +0300

    Initial Commit

---
+++ Odev3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev3
+//
+//  Created by Sinan Sezer on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
